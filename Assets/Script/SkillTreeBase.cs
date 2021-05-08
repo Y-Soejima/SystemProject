78003059c60d620243681237715ff0a4d08da663
YSoejima <gureiosugguto@gmail.com>
2021-05-08 18:33:42 +0900

    SkillTreeBaseを追加

---
+++ Assets/Script/SkillTreeBase.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkillTreeBase : MonoBehaviour
+{
+
+    public enum SkillList
+    {
+        AttackUp,
+        MagicAttackUp,
+        HpUp,
+        MpUp,
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
