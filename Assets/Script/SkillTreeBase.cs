c0cb38c0f5fbb3418ff8dbd116dac8484d1378db
YSoejima <gureiosugguto@gmail.com>
2021-05-08 19:17:16 +0900

    ツリー式にスキルを取得できるか確認した

--- Assets/Script/SkillTreeBase.cs
+++ Assets/Script/SkillTreeBase.cs
@@ -1,10 +1,13 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class SkillTreeBase : MonoBehaviour
 {
-
+    [SerializeField] bool[] isSkillGets = new bool[Enum.GetValues(typeof(SkillList)).Length];
+    int skillNum;
+    bool isSkillGet;
     public enum SkillList
     {
         AttackUp,
@@ -15,11 +18,41 @@ public class SkillTreeBase : MonoBehaviour
 
     void Start()
     {
-        
+        for (int i = 0; i < isSkillGets.Length; i++)
+        {
+            isSkillGets[i] = false;
+        }
     }
 
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            SkillGetChack(0, isSkillGets[0]);
+            SkillGet();
+        }
+        else if (Input.GetMouseButtonDown(1))
+        {
+            SkillGetChack(2, isSkillGets[2]);
+            SkillGet();
+        }
+    }
+
+    void SkillGetChack(int skillNum, bool isSkillGet)
+    {
+        this.skillNum = skillNum;
+        this.isSkillGet = isSkillGet;
+    }
+
+    void SkillGet()
+    {
+        if (skillNum == 0 && !isSkillGet)
+        {
+            isSkillGets[0] = true;
+        }
+        else if (skillNum == 2 && !isSkillGet && isSkillGets[0])
+        {
+            isSkillGets[2] = true;
+        }
     }
 }
