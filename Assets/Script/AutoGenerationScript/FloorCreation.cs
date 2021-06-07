2e490669275d78c085db19e9e88c06ec58e1c426
YSoejima <gureiosugguto@gmail.com>
2021-06-07 15:02:16 +0900

    FloorCreationを追加

---
+++ Assets/Script/AutoGenerationScript/FloorCreation.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloorCreation : MonoBehaviour
+{
+    [SerializeField] int min_x;
+    [SerializeField] int min_y;
+    [SerializeField] int max_x;
+    [SerializeField] int max_y;
+    [SerializeField] int floorNum;
+    [SerializeField] GameObject floorPrefab;
+    void Start()
+    {
+        floorCreate();
+    }
+
+    void floorCreate()
+    {
+
+    }
+}
