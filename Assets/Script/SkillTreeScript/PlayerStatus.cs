9d9e70803b7ac7e6bbf8c5313309e8bc82a4ee7a
YSoejima <gureiosugguto@gmail.com>
2021-07-01 16:59:53 +0900

    レベルアップシステム実装

--- Assets/Script/SkillTreeScript/PlayerStatus.cs
+++ Assets/Script/SkillTreeScript/PlayerStatus.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class PlayerStatus : MonoBehaviour
 {
+    [SerializeField] private int level = 1;
     [SerializeField] private int p_hp = 100;
     [SerializeField] private int p_mp = 100;
     [SerializeField] private int p_atk = 20;
     [SerializeField] private int p_matk = 20;
+    [SerializeField] private int exp;
+    [SerializeField] private int maxExp = 100;
     // Start is called before the first frame update
     void Start()
     {
@@ -38,5 +41,25 @@ public class PlayerStatus : MonoBehaviour
     public void MatkChange(int value)
     {
         p_matk += value;
+    }
+
+    public void ExpUp(int value)
+    {
+        exp += value;
+        if(exp >= maxExp)
+        {
+            exp = exp - maxExp;
+            LevelUp();
+        }
+    }
+
+    public void LevelUp()
+    {
+        level++;
+        maxExp = (int)(maxExp * 1.5);
+        HpChange((int)(p_hp * 0.2));
+        MpChange((int)(p_mp * 0.2));
+        AtkChange((int)(p_atk * 0.2));
+        MatkChange((int)(p_matk * 0.2));
     }
 }
