e9f5ad8e0dff5bdd325e7eb7687a41a8f3bd4e99
YSoejima <gureiosugguto@gmail.com>
2021-07-03 23:30:31 +0900

    コメントを追加

--- Assets/Script/UniRx/UniRxTestObserver.cs
+++ Assets/Script/UniRx/UniRxTestObserver.cs
@@ -6,18 +6,45 @@ using System;
 
 public class UniRxTestObserver : MonoBehaviour
 {
+    /// <summary>
+    /// イベント（UniRx）の宣言
+    /// </summary>
     Subject<int> subject = new Subject<int>();
     // Start is called before the first frame update
     void Start()
     {
-        Observable.Timer(TimeSpan.FromSeconds(2)).Subscribe(_=> subject.Subscribe(x => Debug.Log(x)));
-        
+        int a = 0;
+        //イベントの登録
+        subject.Subscribe(_ => Debug.Log(a));
+        //イベントを発行
+        subject.OnNext(a);
+        //1秒ごとにイベントを発行
+        Observable.Interval(TimeSpan.FromSeconds(1)).Subscribe(_ => subject.OnNext(a++));
+        //subject.OnNext(x);
+        //コルーチンを上から順に発行（実行）
+        Observable.FromCoroutine(CoroutineA)
+        .SelectMany(CoroutineB)
+        .SelectMany(CoroutineA)//SelectManyで合成可能
+        .Subscribe(_ => Debug.Log("All coroutine finished"));
     }
 
     // Update is called once per frame
     void Update()
+    { 
+        
+    }
+
+    IEnumerator CoroutineA()
     {
-        subject.OnNext(100);
-        subject.OnNext(50);
+        Debug.Log("CoroutineA start");
+        yield return new WaitForSeconds(3);
+        Debug.Log("CoroutineA finished");
+    }
+
+    IEnumerator CoroutineB()
+    {
+        Debug.Log("CoroutineB start");
+        yield return new WaitForSeconds(1);
+        Debug.Log("CoroutineB finished");
     }
 }
