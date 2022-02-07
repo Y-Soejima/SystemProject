e9b0217372d9edb993840a824f3bd7df7ec3fe37
YSoejima <gureiosugguto@gmail.com>
2022-02-07 14:41:44 +0900

    データを追加できるようにした

---
+++ Assets/Script/Gacha/AccountCreate.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+using System;
+using UnityEngine.UI;
+using Cysharp.Threading.Tasks;
+using UnityEngine.Networking;
+
+public class AccountCreate : MonoBehaviour
+{
+    [SerializeField] Button button;
+    string url = "https://o1ye3iql8e.execute-api.ap-northeast-1.amazonaws.com/dev/PutItem?UserId=";
+    User user;
+    // Start is called before the first frame update
+    void Start()
+    {
+        user = User.Instance;
+        button.OnClickAsObservable().Subscribe(async _ => await Create(url + user.userId)).AddTo(this);
+    }
+    async UniTask Create(string url)
+    {
+        UnityWebRequest response = UnityWebRequest.Post(url, "");
+        response.SetRequestHeader("Content-Type", "application/json");
+        await response.SendWebRequest();
+        //return response.downloadHandler.text;
+    }
+}
